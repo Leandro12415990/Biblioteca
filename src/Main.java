07c190e0e001f992a2230e12ca79533acdbd1c13
Leandro Monteiro <1241590@isep.ipp.pt>
2024-12-28 18:19:23 +0000

    Menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("=== MENU ===");
+        System.out.println("1. CRUD");
+        System.out.println("2. Criar Reserva");
+        System.out.println("3. Criar Empréstimo");
+        System.out.println("4. Apagar Reserva");
+        System.out.println("5. Pesquisar Livros/Revistas/Jornais pelo ISBN/ISSN");
+        System.out.println("6. Pesquisar Empréstimos e Reservas num intervalo de datas");
+        System.out.println("7. Consultar/alterar a lista de livros de uma reserva ou de um empréstimo");
+        System.out.println("8. Lista de utentes da biblioteca");
+        System.out.println("9. Total de empréstimos realizados num intervalo de datas");
+        System.out.println("10. Tempo médio (em dias) dos empréstimos realizados num intervalo de datas");
+        System.out.println("11. Requisitado (empréstimos e reservas) durante um intervalo de datas");
+        System.out.println("12.  lista dos utentes cuja devolução dos itens emprestados tenha um atraso superior a um número de dias");
+        System.out.println("13. Registar em ficheiro toda a informação");
+        System.out.println("14. Ler de um ficheiro toda a informação");
+        System.out.print("Escolha uma opção: ");
     }
 }
