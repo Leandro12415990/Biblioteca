eb2c3a6cd8de49d0523fb790db76347e489e09ce
Artur Bernardes <1241582@isep.ipp.pt>
2025-01-30 14:54:14 +0000

    commit pesquisa

--- src/Pesquisa.java
+++ src/Pesquisa.java
@@ -2,24 +2,67 @@ import java.util.Scanner;
 
 public class Pesquisa {
 
-    public static void pesquisarLivros(){
+    public static void pesquisarLivros() {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Insira o ISBN: ");
+        System.out.println("\n--- Pesquisa de Livros ---");
+        System.out.print("Insira o ISBN: ");
         String isbn = ler.nextLine();
 
-
+        boolean encontradoLivro = false;
+        for (Livro livro : CRUD.livros) {
+            if (livro.getIsbn().equals(isbn)) {
+                System.out.println("\nLivro encontrado!");
+                System.out.println("Título: " + livro.getTitulo());
+                System.out.println("Editora: " + livro.getEditora());
+                System.out.println("Categoria: " + livro.getCategoria());
+                System.out.println("Ano de Edição: " + livro.getAnoEdicao());
+                System.out.println("ISBN: " + livro.getIsbn());
+                System.out.println("Autor: " + livro.getAutor());
+                encontradoLivro = true;
+                break;
+            }
+        }
     }
 
-    public static void pesquisarJornais(){
+    public static void pesquisarJornais() {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Insira o ISSN: ");
+        System.out.println("\n--- Pesquisa de Jornais ---");
+        System.out.print("Insira o ISSN: ");
         String issn = ler.nextLine();
+
+        boolean encontradoJornal = false;
+        for (Jornal jornal : CRUD.jornais) {
+            if (jornal.getIssn().equals(issn)) {
+                System.out.println("\nJornal encontrado!");
+                System.out.println("Título: " + jornal.getTitulo());
+                System.out.println("Editora: " + jornal.getEditora());
+                System.out.println("Categoria: " + jornal.getCategoria());
+                System.out.println("Data de publicação: " + jornal.getDataPublicacao());
+                System.out.println("ISSN: " + jornal.getIssn());
+                encontradoJornal = true;
+                break;
+            }
+        }
     }
 
-    public static void pesquisarRevistas(){
+    public static void pesquisarRevistas() {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Insira o ISSN: ");
+        System.out.println("\n--- Pesquisa de Revistas ---");
+        System.out.print("Insira o ISSN: ");
         String issn = ler.nextLine();
+
+        boolean encontradoRevista = false;
+        for (Revista revista : CRUD.revistas) {
+            if (revista.getIssn().equals(issn)) {
+                System.out.println("\nJornal encontrado!");
+                System.out.println("Título: " + revista.getTitulo());
+                System.out.println("Editora: " + revista.getEditora());
+                System.out.println("Categoria: " + revista.getCategoria());
+                System.out.println("Data de publicação: " + revista.getDataPublicacao());
+                System.out.println("ISSN: " + revista.getIssn());
+                encontradoRevista = true;
+                break;
+            }
+        }
     }
 }
-
